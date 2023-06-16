f4bf91fb70f6676b2d07b998a2fe821d467e88be
sfrick <s.frick@reply.de>
2023-06-16 08:39:16 +0200

    add filetree

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,102 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		{
+			-- only needed if you want to use the commands with "_with_window_picker" suffix
+			's1n7ax/nvim-window-picker',
+			tag = "v1.5",
+			config = function()
+				require 'window-picker'.setup({
+					autoselect_one = true,
+					include_current = false,
+					filter_rules = {
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { 'terminal', "quickfix" },
+						},
+					},
+					other_win_hl_color = '#e35e4f',
+				})
+			end,
+		}
+	},
+	config = function()
+		require('neo-tree').setup {
+			-- close_if_last_window = true
+			-- A list of functions, each representing a global custom command
+			-- that will be available in all sources (if not overridden in `opts[source_name].commands`)
+			-- see `:h neo-tree-global-custom-commands`
+			commands = {},
+			window = {
+				position = "left",
+				width = 40,
+				mapping_options = {
+					noremap = true,
+					nowait = true,
+				},
+				mappings = {
+					["<space>"] = {
+						"toggle_node",
+						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+					},
+					["<2-LeftMouse>"] = "open",
+					["<cr>"] = "open",
+					["<esc>"] = "revert_preview",
+					["P"] = { "toggle_preview", config = { use_float = true } },
+					["l"] = "focus_preview",
+					["S"] = "open_split",
+					["s"] = "open_vsplit",
+					-- ["S"] = "split_with_window_picker",
+					-- ["s"] = "vsplit_with_window_picker",
+					["t"] = "open_tabnew",
+					-- ["<cr>"] = "open_drop",
+					-- ["t"] = "open_tab_drop",
+					["w"] = "open_with_window_picker",
+					--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+					["C"] = "close_node",
+					-- ['C'] = 'close_all_subnodes',
+					["z"] = "close_all_nodes",
+					--["Z"] = "expand_all_nodes",
+					["a"] = {
+						"add",
+						-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+						config = {
+							show_path = "relative" -- "none", "relative", "absolute"
+						}
+					},
+					["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+					["d"] = "delete",
+					["r"] = "rename",
+					["y"] = "copy_to_clipboard",
+					["x"] = "cut_to_clipboard",
+					["p"] = "paste_from_clipboard",
+					["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+					-- ["c"] = {
+					--  "copy",
+					--  config = {
+					--    show_path = "none" -- "none", "relative", "absolute"
+					--  }
+					--}
+					["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+					["q"] = "close_window",
+					["R"] = "refresh",
+					["?"] = "show_help",
+					["<"] = "prev_source",
+					[">"] = "next_source",
+				}
+			},
+		}
+	end,
+}
